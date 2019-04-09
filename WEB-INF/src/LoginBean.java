5066937bd1d30decd9ae1a8ba4ab421458d887f3
Stanley-Okwii <stanleeparker12@gmail.com>
2019-04-09 16:23:17 +0300

    marvin files

---
+++ WEB-INF/src/LoginBean.java
@@ -0,0 +1,23 @@
+package bean;  
+  
+public class LoginBean {  
+private String Username,pass;  
+  
+public String getUsername() {  
+    return Username;  
+}  
+  
+public void setEmail(String email) {  
+    this.Username = Username;  
+}  
+  
+public String getPass() {  
+    return pass;  
+}  
+  
+public void setPass(String pass) {  
+    this.pass = pass;  
+}  
+  
+  
+}  
---
+++ WEB-INF/src/Provider.java
@@ -0,0 +1,9 @@
+package bean;  
+  
+public interface Provider {  
+String DRIVER="here it will be the driver we are using";  
+String CONNECTION_URL="here its connection url";  
+String USERNAME="for db";  
+String PASSWORD="password for db";  
+  
+}  
